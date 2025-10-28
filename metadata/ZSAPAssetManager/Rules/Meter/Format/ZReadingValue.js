9e5b16e027a402902c627de93ee4de4f59ddcbb2
ssammdksap <ssammdk.sap@gmail.com>
2025-10-28 13:44:47 +0530

    CR Validation for Disconnection and Reconnection read

---
+++ metadata/ZSAPAssetManager/Rules/Meter/Format/ZReadingValue.js
@@ -0,0 +1,21 @@
+import libCom from '../../../../SAPAssetManager/Rules/Common/Library/CommonLibrary';
+
+export default function ReadingValue(context) {
+    let equipment = context.getPageProxy().binding.EquipmentNum;
+    if (context.getPageProxy().binding.BatchEquipmentNum) {
+        equipment = context.getPageProxy().binding.BatchEquipmentNum;
+    }
+    let register = context.binding.RegisterNum;
+    return context.read('/SAPAssetManager/Services/AssetManager.service', 'MeterReadings', ['MeterReadingDate','MeterReadingRecorded'], `$filter=EquipmentNum eq '${equipment}' and Register eq '${register}'&$orderby=MeterReadingDate desc,MeterReadingTime desc`).then(function(result) {
+        if (result && result.length > 0) {
+            if (result.getItem(0).MeterReadingDate !== null) {
+                libCom.setStateVariable(context,'ZPreviousMeterReading', context.formatNumber(result.getItem(0).MeterReadingRecorded));
+                return context.formatNumber(result.getItem(0).MeterReadingRecorded);
+            } else {
+                return '-';
+            }
+        } else {
+            return '-';
+        }
+    });
+}
